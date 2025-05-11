92f1b31c833576a5206294cb181849758d15787a
JoaoVictorCRP <joaovictorxl120@gmail.com>
2025-05-11 12:32:07 -0300

    feat | componente section e cores das bordas

--- comunidade-santa-marta/app/components/Header.js
+++ comunidade-santa-marta/app/components/Header.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Header() {
     return (
         <header className="bg-primary shadow-md pt-1 pb-1 text-black">
-            <div className="max-w-6xl mx-auto flex items-center justify-between text-light">
+            <div className="max-w-6xl mx-auto flex items-center justify-between text-white">
                 <div className="flex items-center space-x-4">
                     <Image
                         src="/logo.png"
@@ -12,6 +12,10 @@ export default function Header() {
                         alt="Comunidade Santa Marta"
                     />
                     <h1 className="text-xl font-bold">Comunidade Santa Marta</h1>
+                </div>
+
+                <div>
+                    <h3>O portal oficial da nossa comunidade!</h3>
                 </div>
             </div>
         </header>
--- comunidade-santa-marta/app/page.js
+++ comunidade-santa-marta/app/page.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import Navbar from "@/app/components/Navbar";
+import Section from "@/app/components/Section";
 
 export default function Home() {
   return (
     <div>
       <Header/>
       <Navbar/>
+      <Section>
+          <p>Oiiiii</p>
+      </Section>
     </div>
   );
 }
