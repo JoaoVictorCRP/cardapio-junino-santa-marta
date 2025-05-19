84d38dad3755a00cb88d4ea67b4a229d5c172100
JoaoVictorCRP <joaovictorxl120@gmail.com>
2025-05-18 22:35:13 -0300

    feat | adicionando cards no painel principal

---
+++ comunidade-santa-marta/app/components/Card.js
@@ -0,0 +1,17 @@
+export default function Card(props) {
+    return (
+        <div
+            className="border-l-smooth border-r-smooth border-t-primary border-b-secondary
+            border-4 rounded-sm mx-3 text-center p-3"
+        >
+            <div className="flex items-center justify-center mb-3 pt-5">
+                {props.icon}
+            </div>
+            <div className="px-1">
+                <h3 className="text-xl font-bold text-secondary">{props.title}</h3>
+                <p className="text-xl font-medium">{props.description}</p>
+            </div>
+
+        </div>
+    )
+}
--- comunidade-santa-marta/app/page.js
+++ comunidade-santa-marta/app/page.js
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Navbar from "@/app/components/Navbar";
 import Section from "@/app/components/Section";
+import Card from "@/app/components/Card";
+import { FaBook, FaCamera, FaCalendarAlt, FaFlag, FaEnvelope } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -8,7 +10,38 @@ export default function Home() {
       <Header/>
       <Navbar/>
       <Section>
-          <p>Oiiiii</p>
+          <div className="flex">
+              <Card
+                  icon={<FaBook size={50} color={"#d32f2f"}/>}
+                  title="Nossa História"
+                  description="Conheça a trajetória e os marcos da nossa comunidade."
+              />
+
+              <Card
+                  icon={<FaCamera size={50} color={"#d32f2f"}/>}
+                  title="Fotos"
+                  description="Confira os principais momentos em registros fotográficos."
+              />
+
+              <Card
+                  icon={<FaCalendarAlt size={50} color={"#d32f2f"}/>}
+                  title="Programação"
+                  description="Veja a agenda de eventos religiosos, encontros e celebrações."
+              />
+
+              <Card
+                  icon={<FaFlag size={50} color={"#d32f2f"}/>}
+                  title="Eventos"
+                  description="Detalhes dos eventos e festas da nossa comunidade."
+              />
+
+              <Card
+                  icon={<FaEnvelope size={50} color={"#d32f2f"}/>}
+                  title="Contato"
+                  description="Encontre endereço, telefone e WhatsApp da comunidade."
+              />
+          </div>
+
       </Section>
     </div>
   );
