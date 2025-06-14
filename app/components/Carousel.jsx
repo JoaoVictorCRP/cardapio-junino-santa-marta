57bb2dab2cbee626340db7e88176b6e86044687a
JoaoVictorCRP <joaovictorxl120@gmail.com>
2025-06-13 23:42:23 -0300

    feat | setinhas no carousel

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -1,15 +1,16 @@
 "use client"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
+import {useRef} from "react";
 
 export default function Carousel({ children }) {
-    const [sliderRef] = useKeenSlider({
+    const [sliderRef, slider] = useKeenSlider({
         loop: true,
         mode: "snap",
         draggable: true,
         slides: {
             perView: 4,
-            spacing: 2,
+            spacing: 8,
             breakpoints: {
                 "(min-width: 640px)": {
                     perView: 2.2,
@@ -25,12 +26,33 @@ export default function Carousel({ children }) {
     })
 
     return (
-        <div ref={sliderRef} className="keen-slider px-2">
-            {children.map((child, index) => (
-                <div className="keen-slider__slide min-w-2xl" key={index}>
-                    {child}
-                </div>
-            ))}
+        <div className="relative w-full">
+            {/* Botão Esquerdo */}
+            <button
+                onClick={() => slider.current?.prev()}
+                className="absolute left-0 top-1/2 z-10 -translate-y-1/2 bg-white p-2 rounded-full shadow-md hover:bg-gray-100"
+                aria-label="Anterior"
+            >
+                ◀
+            </button>
+
+            {/* Carrossel */}
+            <div ref={sliderRef} className="keen-slider px-2">
+                {children.map((child, index) => (
+                    <div className="keen-slider__slide min-w-2xl" key={index}>
+                        {child}
+                    </div>
+                ))}
+            </div>
+
+            {/* Botão Direito */}
+            <button
+                onClick={() => slider.current?.next()}
+                className="absolute right-0 top-1/2 z-10 -translate-y-1/2 bg-white p-2 rounded-full shadow-md hover:bg-gray-100"
+                aria-label="Próximo"
+            >
+                ▶
+            </button>
         </div>
     )
 }
